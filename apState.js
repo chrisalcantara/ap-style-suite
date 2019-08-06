0845f700507dc0f8786dce2fe88782032af20583
chrisalcantara <chris.a.alcantara@gmail.com>
2019-08-06 17:29:25 -0400

    seperate state files

--- apState.js
+++ apState.js
@@ -1,62 +1,15 @@
-const states = {
-  postal: {
-    AK: 'Alaska',
-    AL: 'Ala.',
-    AR: 'Ark.',
-    AZ: 'Ariz.',
-    CA: 'Calif.',
-    CO: 'Colo.',
-    CT: 'Ct.',
-    DC: 'D.C.',
-    DE: 'Del.',
-    FL: 'Fla.',
-    GA: 'Ga.',
-    HI: 'Hawaii',
-    IA: 'Iowa',
-    ID: 'Idaho',
-    IL: 'Ill.',
-    IN: 'Ind.',
-    KS: 'Kan.',
-    KY: 'Ky.',
-    LA: 'La.',
-    MA: 'Mass.',
-    MD: 'Md.',
-    MI: 'Mich.',
-    MN: 'Minn.',
-    MO: 'Mo.',
-    MS: 'Miss.',
-    MT: 'Mont.',
-    NC: 'N.C.',
-    ND: 'N.D.',
-    NE: 'Neb.',
-    NH: 'N.H.',
-    NJ: 'N.J.',
-    NM: 'N.M.',
-    NV: 'Nev.',
-    NY: 'N.Y.',
-    OH: 'Ohio',
-    OK: 'Okla.',
-    OR: 'Ore.',
-    PA: 'Penn.',
-    RI: 'R.I.',
-    SC: 'S.C.',
-    SD: 'S.D.',
-    TN: 'Tenn.',
-    TX: 'Texas',
-    UT: 'Utah',
-    VA: 'Va.',
-    VT: 'Vt.',
-    WA: 'Wash.',
-    WI: 'Wis.',
-    WV: 'W.Va.',
-    WY: 'Wyoming',
-  },
-};
+const { text } = require('./states/text.js');
+const { postal } = require('./states/postal.js');
 
-function postalToState(postal) {
-  return states.postal[postal];
+function postalToState(state) {
+  return postal[state.toUpperCase()];
+}
+
+function textToState(state) {
+  return text[state.toUpperCase()];
 }
 
 module.exports = {
   postal: postalToState,
+  text: textToState,
 };
